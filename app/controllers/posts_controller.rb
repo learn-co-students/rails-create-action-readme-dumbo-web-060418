f3a52cf571bd30a553fdefc84c761f4df851f7ff
robertexe <robtabares92@gmail.com>
2018-07-02 08:29:34 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,12 @@ class PostsController < ApplicationController
 	def new
 		@post = Post.new
 	end
+
+	def create
+	  @post = Post.new ## saved new post into an instance
+	  @post.title = params[:title] ## used instance and assigned a title it took in thru the form
+	  @post.description = params[:description]
+	  @post.save ## saved instance with new params
+	  redirect_to post_path(@post) # redirected to the saved post show page /posts/:id
+	end
 end
